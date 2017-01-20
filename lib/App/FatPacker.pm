ae85237869ad1f8be65b58c8b90cd8338c8cf33b
Matt S Trout <mst@shadowcat.co.uk>
2017-01-20 10:46:52 +0000

    bump version

--- lib/App/FatPacker.pm
+++ lib/App/FatPacker.pm
@@ -14,7 +14,7 @@ use File::Copy qw(copy);
 use File::Path qw(mkpath rmtree);
 use B qw(perlstring);
 
-our $VERSION = '0.010005'; # 0.10.5
+our $VERSION = '0.010_006'; # 0.10.6
 
 $VERSION = eval $VERSION;
 
